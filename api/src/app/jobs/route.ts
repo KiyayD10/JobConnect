294d3dd97d8b97128a847c3d9cdd811031afbd2e
Robani Diansyah <melinting522@gmail.com>
2025-12-02 23:55:22 +0700

    feat: buat method POST untuk bikin lowongan pekerjaan baru

--- api/src/app/jobs/route.ts
+++ api/src/app/jobs/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import { prisma } from '@/lib/prisma'
 import { Prisma, JobType } from "@/generated/prisma"
+import { authenticateRequest } from "@/lib/middleware"
 
 // Ambil semua lowongan dengan filter (Search, Location, Type)
 export async function GET(reaquest: NextRequest): Promise<NextResponse> {
@@ -59,6 +60,65 @@ export async function GET(reaquest: NextRequest): Promise<NextResponse> {
     } catch (error: unknown) {
         if (error instanceof Error) {
             console.error('Gagal mengambil informasi lowongan pekerjaan::', error.message)
+        }
+        return NextResponse.json(
+            {error: "Terjadi kesalahan pada server"},
+            {status: 500}
+        )
+    }
+}
+
+// Buat method untuk membuat lowongan baru
+export async function POST(request: NextRequest): Promise<NextResponse> {
+    try {
+        // Autentikasi user
+        const auth = authenticateRequest(request)
+        if (auth.error) {
+            return auth.error
+        }
+
+        const { user } = auth
+        const body = await request.json()
+        const { title, company, location, type, salary, description, requirements } = body
+
+        // Validasi field (wajib)
+        if (!title || !company || !location || !description || !requirements) {
+            return NextResponse.json(
+                {error: "Data tidak lengkap (Title, Company, Location, Desc, Req wajib diisi)"},
+                {status: 400}
+            )
+        }
+
+        // Simpan job ke database
+        const job = await prisma.job.create({
+            data: {
+                title,
+                company,
+                location,
+                type: type || 'FULLTIME',
+                salary: salary || null,
+                description,
+                requirements,
+                userId: user.userId,
+            },
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                        role: true,
+                    },
+                },
+            },
+        })
+        return NextResponse.json(
+            {message: "Lowongan berhasil dibuat", job},
+            {status: 201}
+        )
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error('Gagal membuat lowongan pekerjaan::', error.message)
         }
         return NextResponse.json(
             {error: "Terjadi kesalahan pada server"},
