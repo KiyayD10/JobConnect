06535682e6f1920c6be48543e12de8bb5c57fd76
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2025-12-14 15:01:51 +0700

    memasukkan  header dan logo di layout

--- web/src/app/auth/layout.tsx
+++ web/src/app/auth/layout.tsx
@@ -1,7 +1,29 @@
+import Image from 'next/image';
 import React from 'react'
+import styles from './auth.module.css'
 
-export default function AuthLayout() {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div></div>
-  )
+    <>
+      <header className={styles.header}>
+        <Image
+          src="/images/logo.png"
+          alt="Logo JobConnect"
+          width={240}
+          height={80}
+          priority
+        />
+      </header>
+
+      <main className={styles.main}>
+        {children}
+      </main>
+      
+    </>
+  );
 }
+
