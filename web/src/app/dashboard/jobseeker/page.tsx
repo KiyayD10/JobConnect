5422616adea004a309e75236489a025df602169a
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2025-12-30 16:34:40 +0700

    membuat dashboard jobseeker

--- web/src/app/dashboard/jobseeker/page.tsx
+++ web/src/app/dashboard/jobseeker/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import DashboardLayout from "../layout";
 
-export default function JobseekerPage() {
+
+
+export default function JobseekerDashboard() {
   return (
-    <div>halaman dasboard jobseeker</div>
+    <DashboardLayout title="Dashboard Jobseeker">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold text-lg">Lowongan Tersedia</h3>
+          <p className="text-3xl font-bold mt-2">124</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold text-lg">Lamaran Dikirim</h3>
+          <p className="text-3xl font-bold mt-2">8</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold text-lg">Diproses</h3>
+          <p className="text-3xl font-bold mt-2">3</p>
+        </div>
+
+      </div>
+    </DashboardLayout>
   )
 }
