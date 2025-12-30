4afcb52a209b296206a856b05816fce8f063ae76
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2025-12-30 16:49:35 +0700

    redirect setelah login

--- web/src/app/auth/login/page.tsx
+++ web/src/app/auth/login/page.tsx
@@ -37,8 +37,17 @@ export default function LoginPage() {
       // simpan auth
       login(json.token, json.user)
 
-      // redirect setelah login
-      router.push("/")
+      // redirect setelah login (BERDASARKAN ROLE)
+      if (json.user.role === "JOBSEEKER") {
+        router.push("/dashboard/jobseeker")
+      } else if (json.user.role === "EMPLOYER") {
+        router.push("/dashboard/employer")
+      } else if (json.user.role === "ADMIN") {
+        router.push("/dashboard/admin")
+      } else {
+        router.push("/auth/login")
+      }
+
     } catch (err) {
       if (err instanceof Error) setError(err.message)
     } finally {
