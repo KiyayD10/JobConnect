eb6adf338a02d565e0cbc88d44a741421d9dd04a
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2025-12-30 16:36:45 +0700

    membuat dashboar admin

--- web/src/app/dashboard/admin/page.tsx
+++ web/src/app/dashboard/admin/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import DashboardLayout from '../layout'
 
 export default function AdminPage() {
   return (
-    <div>halaman dasboard admin</div>
+     <DashboardLayout title="Dashboard Admin">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold">Total User</h3>
+          <p className="text-3xl font-bold mt-2">520</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold">Perusahaan</h3>
+          <p className="text-3xl font-bold mt-2">86</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold">Lowongan</h3>
+          <p className="text-3xl font-bold mt-2">210</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow">
+          <h3 className="font-semibold">Aktivitas Hari Ini</h3>
+          <p className="text-3xl font-bold mt-2">47</p>
+        </div>
+
+      </div>
+    </DashboardLayout>
   )
 }
